4bd6ea1cdb33655c76abde34c1c95c3da0414666
simranrawat <Sivans@26>
2022-05-09 18:04:34 +0530

    Main class

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -1,8 +1,9 @@
 package io.agileintelligence.ppmtool;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PpmtoolApplication {
