c9fa7a8c1f34ff1e17cc74963ddef7fa930c9cc5
simranrawat <Sivans@26>
2022-05-11 22:18:22 +0530

    updated application.properties

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -23,71 +23,69 @@ public class ProjectService {
     @Autowired
     private UserRepository userRepository;
 
-    public Project saveOrUpdateProject(Project project, String username){
+    public Project saveOrUpdateProject(Project project, String username) {
 
-        if(project.getId() != null){
+        if (project.getId() != null) {
             Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
-            if(existingProject !=null &&(!existingProject.getProjectLeader().equals(username))){
+            if (existingProject != null && (!existingProject.getProjectLeader().equals(username))) {
                 throw new ProjectNotFoundException("Project not found in your account");
-            }else if(existingProject == null){
-                throw new ProjectNotFoundException("Project with ID: '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+            } else if (existingProject == null) {
+                throw new ProjectNotFoundException("Project with ID: '" + project.getProjectIdentifier()
+                        + "' cannot be updated because it doesn't exist");
             }
         }
 
-        try{
+        try {
 
             User user = userRepository.findByUsername(username);
             project.setUser(user);
             project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
-            if(project.getId()==null){
+            if (project.getId() == null) {
                 Backlog backlog = new Backlog();
                 project.setBacklog(backlog);
                 backlog.setProject(project);
                 backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             }
 
-            if(project.getId()!=null){
-                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            if (project.getId() != null) {
+                project.setBacklog(
+                        backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
             }
 
             return projectRepository.save(project);
 
-        }catch (Exception e){
-            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        } catch (Exception e) {
+            throw new ProjectIdException(
+                    "Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
 
     }
 
+    public Project findProjectByIdentifier(String projectId, String username) {
 
-    public Project findProjectByIdentifier(String projectId, String username){
-
-        //Only want to return the project if the user looking for it is the owner
+        // Only want to return the project if the user looking for it is the owner
 
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
-        if(project == null){
-            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
 
         }
 
-        if(!project.getProjectLeader().equals(username)){
+        if (!project.getProjectLeader().equals(username)) {
             throw new ProjectNotFoundException("Project not found in your account");
         }
-
-
 
         return project;
     }
 
-    public Iterable<Project> findAllProjects(String username){
+    public Iterable<Project> findAllProjects(String username) {
         return projectRepository.findAllByProjectLeader(username);
     }
 
-
-    public void deleteProjectByIdentifier(String projectid, String username){
-
+    public void deleteProjectByIdentifier(String projectid, String username) {
 
         projectRepository.delete(findProjectByIdentifier(projectid, username));
     }
